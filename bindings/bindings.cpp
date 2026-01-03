20e881bd155680a0fbd927e908c89c57a2424f73
AlexTriedLife <alexmcphersonuribe@gmail.com>
2026-01-03 16:12:12 -0500

    feat(bindings): setup bindings to use library in python

---
+++ bindings/bindings.cpp
@@ -0,0 +1,26 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/operators.h>
+#include "amumath/Vector3.h"
+
+namespace py = pybind11;
+using namespace amumath;
+
+PYBIND11_MODULE(amumath_py, m) {
+    m.doc() = "AMUMath Python Plugin";
+
+    py::class_<Vector3>(m, "Vector3")
+        .def(py::init<float, float, float>(), py::arg("x")=0, py::arg("y")=0, py::arg("z")=0)
+        .def_readwrite("x", &Vector3::x)
+        .def_readwrite("y", &Vector3::y)
+        .def_readwrite("z", &Vector3::z)
+        .def("dot", &Vector3::Dot)
+        .def("magnitude", &Vector3::Magnitude)
+        .def("normalize", &Vector3::Normalize)
+        .def_static("cross", &Vector3::Cross)
+        .def(py::self + py::self) 
+        .def(py::self == py::self)
+        .def(py::self * float()) // v * s
+        .def(float() * py::self) // s * v
+        .def(py::self / float())
+        .def("__repr__", &Vector3::ToString);
+}
