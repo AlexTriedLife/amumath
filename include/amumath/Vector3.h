7989635a2b97b53372ea38229acee745482f8f2b
AlexTriedLife <alexmcphersonuribe@gmail.com>
2026-01-03 14:39:55 -0500

    feat(vec3): added cross product, scalar division, magnitude
    Next: create normalize and test

--- include/amumath/Vector3.h
+++ include/amumath/Vector3.h
@@ -13,18 +13,16 @@ struct Vector3 {
   // Vector addition
   Vector3 operator+(const Vector3 &other) const;
 
-  // ToDo: division, cross
   // Equality u == v
   bool operator==(const Vector3 &other) const;
 
   // Inequality u != v
   bool operator!=(const Vector3 &other) const;
 
-  // Scalar Multiplication s * v
+  // Scalar Multiplication v * s (s * v does not work as a member function) -> defined as free function
   Vector3 operator*(const float s) const;
 
-  // TODO: scalar division
-  // TODO: Cross product <Make a matrix first>
+  static  Vector3 Cross(const Vector3 &u, const Vector3 &v);
   Vector3 operator/(const float s) const;
   // Make vector negative
   Vector3 operator-() const;
@@ -32,12 +30,23 @@ struct Vector3 {
   Vector3 operator-(const Vector3 &other) const;
 
   // Function operations
-  float dot(const Vector3 &other) const;
+  float Dot(const Vector3 &other) const;
+  float Magnitude() const;
+
+  // Helper functions
+  float LengthSquared() const;
 
 };
 
 // Dot Product as a free function
 float operator*(const Vector3 &u, const Vector3 &v);
+
+
+// Commuative operator overloads
+Vector3 operator*(float s,  const Vector3 &v);
+
+// Non-member helper Function
+float LengthSquared(const Vector3& u);
 
 } // namespace amumath
 
--- src/Vector3.cpp
+++ src/Vector3.cpp
@@ -1,11 +1,12 @@
-#include "amumath/Vector2.h"
+#include "amumath/Vector3.h"
 #include <cmath>
 
 namespace amumath {
 // 3 arg constructor
 Vector3::Vector3(float x, float y, float z) : x(x), y(y), z(z) {}
 
-// Operator overloads
+
+// Operator overloads (member functions)
 Vector3 Vector3::operator+(const Vector3 &other) const {
   return {x + other.x, y + other.y, z + other.z};
 }
@@ -32,10 +33,44 @@ Vector3 Vector3::operator*(const float s) const {
   return {x * s, y * s, z * s};
 }
 
-float operator*(const Vector3 &u, const Vector3 &v) { return u.dot(v); }
+Vector3 Vector3::operator/(const float s) const {
+    return {x / s, y / s, z / s};
+}
 
-float Vector3::dot(const Vector3 &other) const {
+
+// Member functions
+float Vector3::Dot(const Vector3 &other) const {
   return (x * other.x) + (y * other.y) + (z * other.z);
+}
+
+Vector3 Vector3::Cross(const Vector3 &u, const Vector3 &v) {
+  return {u.y * v.z - u.z * v.y, u.z * v.x - u.x * v.z, u.x * v.y - u.y * v.x};
+}
+
+float Vector3::LengthSquared() const {
+    return x * x + y * y + z * z;
+}
+
+float Vector3::Magnitude() const {
+    return std::sqrt(LengthSquared());
+}
+
+
+// Helpers
+float LengthSquared(const Vector3 &u)   {
+    return {
+        u.x * u.x +
+        u.y * u.y +
+        u.z * u.z
+    };
+}
+
+// Free functions
+// Commutative operations
+float operator*(const Vector3 &u, const Vector3 &v) { return u.Dot(v); }
+// Scalar mult s * v
+Vector3 operator*(float s, const Vector3 &v) {
+    return v * s;
 }
 
 } // namespace amumath
